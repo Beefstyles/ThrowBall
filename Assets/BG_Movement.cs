d5bfcee814b8744b4badbf92a4c58b57135d368b
Beefstyles <glenwillis@gmail.com>
2015-02-03 19:17:21 +0800

    Added a main menu and a background

---
+++ Assets/BG_Movement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG_Movement : MonoBehaviour {
+
+	public GameObject BGSprite;
+	public float rotationSpeed;
+
+	void Update () {
+		BGSprite.transform.Rotate (Vector3.forward * Time.deltaTime * rotationSpeed, Space.World);
+	
+		}
+	}
+
+
